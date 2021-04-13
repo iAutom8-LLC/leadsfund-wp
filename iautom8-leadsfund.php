bc2f76d309cee6936f02635c361045a85adb3e6a
Jarrod Noonan <jarrod@georgias-mbp.home>
2021-04-13 21:08:12 +0100

    Update project description

--- iautom8-leadsfund.php
+++ iautom8-leadsfund.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       Leads Fund Integration
  * Plugin URI:        https://i-autom8.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Description:       Add and utilise Leads Fund embedded buttons
+ * Version:           1.0.1
  * Author:            iAutoM8
  * Author URI:        https://i-autom8.com
  * License:           GPL-2.0+
@@ -37,7 +37,7 @@ require_once( __DIR__ . '/vendor/autoload.php' );
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'IAUTOM8_CC_VERSION', '1.0.0' );
+define( 'IAUTOM8_CC_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
